12f68e20c6a1c834d29df46f0ed0174d07970b4c
MarFedorovna <91743581+MarFedorovna@users.noreply.github.com>
2021-11-29 00:34:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+def sort_sentence(sentence):
+    list_of_words = sorted(sentence.split(), key=len)
+    new_string_with_lower_char = ' '.join(list_of_words).lower()
+    answer_string = new_string_with_lower_char[0].upper() + new_string_with_lower_char[1:]
+    return answer_string
 
+if __name__ == '__main__':
+    s = input("Введите строку для сортировки: \n")
+    print(sort_sentence(s))
